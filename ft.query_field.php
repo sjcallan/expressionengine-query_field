2a97aaf26273fdb04670f76ce571b2dae32000c5
Steve Callan <steve@sjcallandesigns.com>
2011-04-04 09:36:22 -0400

    Original base file
    Signed-off-by: Steve Callan <steve@sjcallandesigns.com>

---
+++ ft.query_field.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Query_field_ft extends EE_Fieldtype {
+
+	var $info = array(
+		'name'		=> 'Query Field',
+		'version'	=> '1.0'
+	);
+	
+	// --------------------------------------------------------------------
+	
+	function display_field($data)
+	{
+		
+		
+	}
+	
+	
+}
+// END Query_field_ft class
+
+/* End of file ft.query_field.php */
